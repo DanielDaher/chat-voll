8f42c94bffe3b87b10bc3575ea5e3befa1527be4
Daniel <danielfdoficial@gmail.com>
2022-01-27 16:07:17 -0300

    create: add state to both components (register and login) to get input values

--- pages/Login.js
+++ pages/Login.js
@@ -1,11 +1,24 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <nav className='login'>
       <h1>Chat Voll</h1>
       <p>Acesse sua conta para entrar no chat!</p>
       <form>
-        <input type='text' />
-        <input type='password' />
+        <input
+          type='text'
+          placeholder="Insira seu nome de usuário"
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <input 
+          type='password'
+          placeholder="Insira sua senha"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button>Entrar</button>
       </form>
     </nav>
--- pages/Register.js
+++ pages/Register.js
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
 export default function Register() {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <main className='register'>
       <h1>Crie sua conta agora mesmo</h1>
@@ -7,10 +12,12 @@ export default function Register() {
         <input
           type='text'
           placeholder="Insira seu nome de usuário"
+          onChange={(e) => setUserName(e.target.value)}
         />
         <input
           type='password'
           placeholder="Insira sua senha"
+          onChange={(e) => setPassword(e.target.value)}
         />
         <button>Entrar</button>
       </form>
