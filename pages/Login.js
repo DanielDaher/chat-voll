e370dbac06a7f21040b24d2cdd4e50870086991d
Daniel <danielfdoficial@gmail.com>
2022-01-27 15:21:47 -0300

    create: add login inputs

--- pages/Login.js
+++ pages/Login.js
@@ -1,7 +1,13 @@
 export default function Login() {
   return (
     <nav className='login'>
-      <h1>Login</h1>
+      <h1>Chat Voll</h1>
+      <p>Acesse sua conta para entrar no chat!</p>
+      <form>
+        <input type='text' />
+        <input type='password' />
+        <button>Entrar</button>
+      </form>
     </nav>
   );
 }
