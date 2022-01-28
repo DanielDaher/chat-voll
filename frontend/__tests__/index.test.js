81dce82171d53708a4e0390fd94dda3813a70bb8
Daniel <danielfdoficial@gmail.com>
2022-01-27 23:02:27 -0300

    test: add password input tests to homepage

--- frontend/__tests__/index.test.js
+++ frontend/__tests__/index.test.js
@@ -3,11 +3,28 @@ import userEvent from '@testing-library/user-event'
 import Home from '../pages/index'
 
 describe('Home', () => {
-  it('renders a heading', () => {
-    render(<Home />)
+  it('testa se a homepage apresenta 2 inputs para o usuário inserir seu username', () => {
+    render(<Home />);
 
     const userNameInputs = screen.getAllByPlaceholderText('Insira seu nome de usuário');
 
     expect(userNameInputs).toHaveLength(2);
-  })
+  });
+
+  it('testa se a homepage apresenta 2 inputs para o usuário inserir sua senha', () => {
+    render(<Home />);
+
+    const passwordInputs = screen.getAllByPlaceholderText('Insira sua senha');
+
+    expect(passwordInputs).toHaveLength(2);
+  });
+
+  it('testa se, ao realizar o cadastro correto, o usuário é redirecionato para a tela webchat', () => {});
+
+  it('testa se, ao realizar o cadastro INCORRETO, aparece uma mensagem de erro na tela', () => {});
+  
+  it('testa se, ao realizar o login correto, o usuário é redirecionato para a tela webchat', () => {});
+
+  it('testa se, ao realizar o login INCORRETO, aparece uma mensagem de erro na tela', () => {});
+
 })
