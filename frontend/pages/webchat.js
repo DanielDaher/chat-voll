6e0c61ca069c1804c2129aea775af9e619c38eee
Daniel <danielfdoficial@gmail.com>
2022-01-29 14:06:43 -0300

    create: add logic to webchat.js redirect if user is not logged

--- frontend/pages/webchat.js
+++ frontend/pages/webchat.js
@@ -1,4 +1,12 @@
+import { useEffect } from "react";
+import Router from 'next/router';
+
 export default function Webchat() {
+  useEffect(() => {
+    if (!sessionStorage.getItem('vollChatToken')) {
+      Router.push('/');
+    }
+  }, []);
   return (
     <h1>WEBCHAT</h1>
   );
